566865e12e44f51d25cabcfc64b62117c7c688bc
Mary Hall <mh6uj@students-MacBook-Pro.local>
2017-12-07 20:21:23 -0500

    button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,32 @@
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBvR72yR3YHOJ2eZySRtEhuV70rrpzm4TU",
-    authDomain: "employeedatamanagement-f4a39.firebaseapp.com",
-    databaseURL: "https://employeedatamanagement-f4a39.firebaseio.com",
-    projectId: "employeedatamanagement-f4a39",
-    storageBucket: "employeedatamanagement-f4a39.appspot.com",
-    messagingSenderId: "1035319251259"
-  };
-  firebase.initializeApp(config);
+  // // Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyBvR72yR3YHOJ2eZySRtEhuV70rrpzm4TU",
+  //   authDomain: "employeedatamanagement-f4a39.firebaseapp.com",
+  //   databaseURL: "https://employeedatamanagement-f4a39.firebaseio.com",
+  //   projectId: "employeedatamanagement-f4a39",
+  //   storageBucket: "employeedatamanagement-f4a39.appspot.com",
+  //   messagingSenderId: "1035319251259"
+  // };
+  // firebase.initializeApp(config);
+
+  // //reference to database
+  // var database = firebase.database();
+
+  // //Initial variables
+
+  // var currentEmployees
+$(document).ready(function() {
+   $("#awesomeButton").on('click', function(event){
+    event.preventDefault();
+
+    alert("Button got pushed!");
+
+    var employeeName = $("#empName").val().trim();
+    var role = $("#role").val().trim();
+
+    console.log(employeeName);
+    console.log(role);
+  }); 
+ });
+
