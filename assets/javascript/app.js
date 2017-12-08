a74fdcd934cdbfd67281dc4404ad88b94ab40bdb
Brittany <brittanygillespie31@gmail.com>
2017-12-07 19:18:46 -0500

    firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBvR72yR3YHOJ2eZySRtEhuV70rrpzm4TU",
+    authDomain: "employeedatamanagement-f4a39.firebaseapp.com",
+    databaseURL: "https://employeedatamanagement-f4a39.firebaseio.com",
+    projectId: "employeedatamanagement-f4a39",
+    storageBucket: "employeedatamanagement-f4a39.appspot.com",
+    messagingSenderId: "1035319251259"
+  };
+  firebase.initializeApp(config);
